86ba9f0fe7823483f18d2ad261e91e91be6cbd24
Songmu <y.songmu@gmail.com>
2015-09-21 17:15:00 +0900

    change parsing of command line arguments

--- cmd/enslaver/main.go
+++ cmd/enslaver/main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/Songmu/enslaver"
 )
 
 func main() {
-	enslaver.Command(os.Args[1], os.Args[2:]...).Run()
+	l := len(os.Args)
+	opts := []string{}
+	args := []string{}
+	for i, v := range os.Args {
+		if v == "--" && i+1 < l {
+			args = os.Args[i+1:]
+			break
+		}
+		opts = append(opts, v)
+	}
+	if len(args) == 0 {
+		opts = []string{}
+		args = os.Args[1:]
+	}
+	if len(args) == 0 {
+		fmt.Fprintln(os.Stderr, "No commands are specified")
+		os.Exit(1)
+	}
+
+	enslaver.Command(args[0], args[1:]...).Run()
 }
